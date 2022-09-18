fb835b9733ef079a5de94a806efca1a3cb4d3910
Xiao Qiang <goldenpig951204@gmail.com>
2022-09-19 04:31:54 +0800

    multi-proxy complete

--- src/routes/semrush.js
+++ src/routes/semrush.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { memberMiddleware } = require('../middlewares/permission');
-const {wwwProxy, esProxy, deProxy, frProxy, itProxy, ptProxy, viProxy, trProxy, zhProxy, jaProxy, koProxy } = require('../middlewares/semrushProxy');
+const semrushProxy = require('../middlewares/semrushProxy');
 const semrushRouter = express.Router();
 const config = require('../services/config');
 /**
@@ -9,18 +9,7 @@ const config = require('../services/config');
 
 applyMiddleware = (req, res, next) => {
     let locale = config.getConfig()["locale"] ? config.getConfig()["locale"]: "www";
-    if (locale == "www") return wwwProxy(req, res, next);
-    else if (locale == "es") return esProxy(req, res, next);
-    else if (locale == "de") return deProxy(req, res, next);
-    else if (locale == "fr") return frProxy(req, res, next);
-    else if (locale == "it") return itProxy(req, res, next);
-    else if (locale == "pt") return ptProxy(req, res, next);
-    else if (locale == "vi") return viProxy(req, res, next);
-    else if (locale == "tr") return trProxy(req, res, next);
-    else if (locale == "zh") return zhProxy(req, res, next);
-    else if (locale == "ja") return jaProxy(req, res, next);
-    else if (locale == "ko") return koProxy(req, res, next);
-    else return wwwProxy(req, res, next);
+    return semrushProxy(locale)(req, res, next);
 }
 semrushRouter.use('/', memberMiddleware, applyMiddleware);
 
